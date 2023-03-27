17231a39e65b6803b41e770f376a36c866661bd8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-27 09:36:16 +0300

    js exercises

---
+++ script.js
@@ -0,0 +1,10 @@
+// exercise-1
+
+let myFavFood = 'mac&cheese';
+let myFavMeal = 'breakfast';
+
+console.log(`I eat ${myFavFood} at every ${myFavMeal}.`)
+
+// exercise-2
+
+const myWatchedSeries = ['black mirror', 'money heist', 'the big bang theory'];
